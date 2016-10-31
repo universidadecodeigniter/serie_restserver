f6e313bb102b23aa64260ddbd8aeacdc19697b0d
jlamim <dev@jlamim.com.br>
2016-10-31 16:35:23 -0200

    Ajuste das rotas

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,5 +7,3 @@ $route['translate_uri_dashes']          = TRUE;
 
 $route["api/usuarios"]                  = "api/Usuarios";
 $route["api/usuarios/(:num)"]           = "api/Usuarios";
-//$route["api/usuarios/(:num)"]["put"]    = "api/Usuarios/id/$1";
-//$route["api/usuarios/(:num)"]["delete"] = "api/Usuarios/id/$1";
